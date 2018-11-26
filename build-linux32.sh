153210010845f60b981eef0c546886b7de51b970
bitbegin <bitbegin@gmail.com>
2018-11-26 11:25:03 +0800

    add build linux32

---
+++ build-linux32.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -x
+
+build_dll() {
+	./autogen.sh
+	echo "LDFLAGS = -no-undefined" >> Makefile.am
+	./configure --host=$1 --enable-module-recovery --enable-experimental --enable-module-ecdh --enable-endomorphism --disable-jni
+	make
+}
+
+# make clean
+build_dll x86_64-linux-gnu
+
+cp .libs/libsecp256k1.so ../
+cp .libs/libsecp256k1.so.0 ../
+cp .libs/libsecp256k1.so.0.0.0 ../
+
+# ./autogen.sh
+# echo "LDFLAGS = -no-undefined" >> Makefile.am
+# ./configure --host=x86_64-w64-mingw32 --enable-module-recovery --enable-experimental --enable-module-ecdh --enable-endomorphism --disable-jni
+# ./configure --host=i686-w64-mingw32 --enable-module-recovery --enable-experimental --enable-module-ecdh --enable-endomorphism --disable-jni
