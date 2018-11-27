69b7c9c31bebfdf77ecb8b7c156de01fe3651fb7
bitbegin <bitbegin@bitbegindeMac.local>
2018-11-27 01:49:14 -0800

    update macos script

--- build-macos32.sh
+++ build-macos32.sh
@@ -5,12 +5,12 @@ set -e -x
 build_dll() {
 	./autogen.sh
 	echo "LDFLAGS = -no-undefined" >> Makefile.am
-	./configure --host=$1 --enable-module-recovery --enable-experimental --enable-endomorphism --disable-jni
+	./configure --host=$1 --enable-module-recovery --enable-experimental --enable-endomorphism --disable-jni --disable-benchmark --disable-tests
 	make
 }
 
 # make clean
-build_dll x86_64-linux-gnu
+build_dll i686-apple-darwin17.6.0
 
 cp .libs/libsecp256k1.so ../libsecp256k1.dylib
 
